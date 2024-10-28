9e45da2d87719f763cd6d58336b89993cbf9b727
HEISENYAN <21104095d@connect.polyu.hk>
2024-10-28 22:02:42 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import session from "express-session"
+const app = express()
+const path = "static"
+app.use(
+    session({
+        secret:"EIE4432_project",
+        resave:false,
+        saveUninitialized:false,
+        cookie:{httpOnly:true, masAge:60000}
+    })
+)
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:false
+}));
+app.use('/',express.static(path)) // Use static files in /static
+
+app.listen(8080)
