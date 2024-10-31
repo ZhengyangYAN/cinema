72b9f37b1ea73901a062b2e1a3e227c4a43a8615
HEISENYAN <21104095d@connect.polyu.hk>
2024-10-31 16:34:57 +0800

    update on hyper link

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ app.use(express.urlencoded({
     extended:false
 }));
 app.use('/',express.static(path)) // Use static files in /static
-
+app.use('/images',express.static("images"))
 app.listen(8080)
