a327dabc078ffc05516c06b74b12c8007d207e0d
22098552d <22098552d@connect.polyu.hk>
2024-11-21 15:26:36 +0800

    seat-selection(uniform price)

--- static/js/seat-selection.js
+++ static/js/seat-selection.js
@@ -1,7 +1,6 @@
 jQuery(function() {
     //localStorage.removeItem("bookedTable");
     let bookingInfo = JSON.parse(localStorage.getItem("bookedTable")) || [];
-    let selectedTable = null;
     updateAppearence();
 
     function updateAppearence(){
@@ -18,6 +17,7 @@ jQuery(function() {
             if($(this).hasClass('selected')) $(this).removeClass('selected');
             else $(this).addClass('selected');
         }
+        calculatePrice();
     });
 
     $("#Confirm").on("click", function(){
@@ -27,13 +27,25 @@ jQuery(function() {
             if ($(this).hasClass('selected')) {
                 $(this).addClass('booked');
                 $(this).removeClass('selected');
-                alert($(this).index());
+                //alert($(this).index());
                 bookingInfo.push($(this).index());
                 localStorage.setItem("bookedTable", JSON.stringify(bookingInfo));
             }
         });
         updateAppearence();
     });
+
+    function calculatePrice(){
+        //var size = document.querySelector('input[name="size"]:checked');
+        //var drink = document.getElementById('drinkSelection').value;
+        var price = 0;
+        $('.seat').each(function() {
+            if($(this).hasClass('seat') && !$(this).hasClass('booked')){
+                if($(this).hasClass('selected')) price += 50;
+            }
+        });
+        $('#price').text(price);
+    }
 
     updateAppearence();
 });
